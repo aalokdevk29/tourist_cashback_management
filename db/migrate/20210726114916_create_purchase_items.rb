ba46a3d61732e97e7503ae9cfd8a7eb47c379fed
Aalok Dev <aalokdevk29@gmail.com>
2021-07-27 16:01:25 +0530

    Set default values

--- db/migrate/20210726114916_create_purchase_items.rb
+++ db/migrate/20210726114916_create_purchase_items.rb
@@ -4,9 +4,9 @@ class CreatePurchaseItems < ActiveRecord::Migration[6.1]
       t.references :purchase
       t.references :ticket
       t.integer :quantity
-      t.decimal :unit_price, precision: 8, scale: 2
-      t.decimal :total, precision: 8, scale: 2
-      t.decimal :cashback, precision: 8, scale: 2
+      t.decimal :unit_price, precision: 8, scale: 2, default: 0
+      t.decimal :total, precision: 8, scale: 2, default: 0
+      t.decimal :cashback, precision: 8, scale: 2, default: 0
       t.timestamps
     end
   end
